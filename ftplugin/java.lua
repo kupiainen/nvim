a248120cf8e1affe4ba53de6fa48ed8abf6f727a
kupiainen <thepeexu@gmail.com>
2024-03-27 11:30:08 -0400

    .class files error in opening after config transf.

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -11,3 +11,6 @@
 --
 -- more info about new lsp-zero config:
 -- https://gist.github.com/slinky07/e5a4cd5b399f48227e658261160fe20c
+--
+-- after the config change to lsp, .class files do not open anymore
+-- with neovim
