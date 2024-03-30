32a8fdca68fcf332f72f4ff70a83c1d81ef9463d
kupiainen <thepeexu@gmail.com>
2024-03-30 17:23:54 -0400

    Improved java lsp functionalities

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,11 @@
-local config = {
-    cmd = {"C:/Users/peetu/AppData/Local/nvim-data/mason/bin/jdtls.CMD"},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-}
-require('jdtls').start_or_attach(config)
+-- local config = {
+--     cmd = {"C:/Users/peetu/AppData/Local/nvim-data/mason/bin/jdtls.CMD"},
+--     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+-- }
+-- require('jdtls').start_or_attach(config)
+--
+-- more info about new lsp-zero config:
+-- https://gist.github.com/slinky07/e5a4cd5b399f48227e658261160fe20c
+--
+-- after the config change to lsp, .class files do not open anymore
+-- with neovim
