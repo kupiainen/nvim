bbda94b37fbd9557477f58497e2dff4bdb83236d
kupiainen <thepeexu@gmail.com>
2024-04-09 00:11:33 -0400

    bind for | to alt-ctrl-a

--- lua/poweqconfig/remap.lua
+++ lua/poweqconfig/remap.lua
@@ -16,6 +16,8 @@ vim.keymap.set("v", "<Leader>'","c'<C-r>-'<Esc>")
 
 vim.keymap.set("i", "§", "<")
 vim.keymap.set("i", "½", ">")
+vim.keymap.set("i", "ə", "|")
+
 
 -- copy to system clipboard !!MUST HAVE 'xclip' or equivalent!!
 vim.keymap.set('v', '<leader>y', '"+y')
